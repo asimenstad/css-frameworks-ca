4304ff9f653ce33dd5797b8fd5b03c9397c64dce
asimenstad <a-nna-s@hotmail.com>
2022-09-01 11:17:16 +0200

    Added footer, fixed layout

--- src/js/add-contacts.js
+++ src/js/add-contacts.js
@@ -2,7 +2,7 @@ const contactContainer = document.querySelector(".container.contacts");
 
 function addContacts() {
   for (let i = 0; i < contacts.length; i++) {
-    contactContainer.innerHTML += `<div class="d-flex flex-column-reverse flex-md-row-reverse align-items-center justify-content-end gap-2"><h3 >${contacts[i].name}</h3><img class="img-fluid  rounded-circle thumbnail" src="${contacts[i].image}"></div>`;
+    contactContainer.innerHTML += `<div class="d-flex flex-column-reverse flex-md-row-reverse align-items-center justify-content-end gap-2"><h3 class="mt-md-2">${contacts[i].name}</h3><img class="img-fluid rounded-circle thumbnail" src="${contacts[i].image}"></div>`;
   }
 }
 addContacts();
